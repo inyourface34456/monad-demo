731d5e52f2d967ba6f3c6a0f05ed59fe91fa38b5
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-12-19 11:04:26 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,39 +22,49 @@ where
         self.base
     }
 
-    fn add_logs(&mut self, opp_desc: String, curr_num: T) {
-        self.logs.push(opp_desc);
+    fn add_logs(&mut self, opp_desc: &str, curr_num: T) {
+        self.logs.push(opp_desc.to_string());
         self.trace.push(curr_num);
     }
 }
 
-fn square<T: std::ops::Mul<Output = T> + std::marker::Copy>(i: &T) -> MonadWrapper<T>
+fn square<T: std::marker::Copy>(i: &T) -> MonadWrapper<T>
 where
     for<'a> &'a T: std::ops::Mul<&'a T, Output = T>,
 {
     let new_num = i * i;
     let mut seg = MonadWrapper::new(new_num);
-    seg.add_logs("Squared the number".to_string(), new_num);
+    seg.add_logs("Squared the number", new_num);
     seg
 }
 
-fn times_two<T: std::ops::Add<Output = T> + std::marker::Copy>(i: &T) -> MonadWrapper<T>
+fn times_two<T: std::marker::Copy>(i: &T) -> MonadWrapper<T>
 where
     for<'a> &'a T: std::ops::Add<&'a T, Output = T>,
 {
     let new_num = i + i;
     let mut seg = MonadWrapper::new(new_num);
-    seg.add_logs("Multplied the number by two".to_string(), new_num);
+    seg.add_logs("Multplied the number by two", new_num);
     seg
 }
 
-fn add_one<T: std::ops::Add<Output = T> + std::marker::Copy>(i: &T) -> MonadWrapper<T>
+fn add_one<T: std::marker::Copy>(i: &T) -> MonadWrapper<T>
 where
     for<'a> &'a T: std::ops::Add<u128, Output = T>,
 {
     let new_num = i + 1;
     let mut seg = MonadWrapper::new(new_num);
-    seg.add_logs("Added one to the number".to_string(), new_num);
+    seg.add_logs("Added one to the number", new_num);
+    seg
+}
+
+fn divide_by_two<T: std::marker::Copy>(i: &T) -> MonadWrapper<T>
+where
+    for<'a> &'a T: std::ops::Div<u128, Output = T>,
+{
+    let new_num = i / 2;
+    let mut seg = MonadWrapper::new(new_num);
+    seg.add_logs("Devided the number by two", new_num);
     seg
 }
 
@@ -78,25 +88,20 @@ fn main() {
     let mut first = transform(number, add_one);
     let mut rng = rand::thread_rng();
     for _ in 1..10 {
-      let x = rng.gen_range(1..=3);
-      match x {
-        1 => {
-           first = transform(first, add_one);
+        let x = rng.gen_range(1..=4);
+        match x {
+            1 => first = transform(first, add_one),
+            2 => first = transform(first, times_two),
+            3 => first = transform(first, square),
+            4 => first = transform(first, divide_by_two),
+            _ => panic!(),
         }
-        2 => {
-           first = transform(first, times_two);
-        }
-        3 => {
-           first = transform(first, square);
-        }
-        _ => panic!()
-      }
     }
 
     println!(
         "Answer is {}\nLogs are: {:?}\nTrace is: {:?}",
-      first.unwrap(),
-      first.logs,
-      first.trace
+        first.unwrap(),
+        first.logs,
+        first.trace
     );
 }
