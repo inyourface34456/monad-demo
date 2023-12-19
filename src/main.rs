1f890189c46549ab05bde82204a55af178268346
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-12-19 10:42:18 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use rand::prelude::*;
+
 struct MonadWrapper<T> {
     base: T,
     logs: Vec<String>,
+    trace: Vec<T>,
 }
 
 impl<T> MonadWrapper<T>
@@ -8,15 +11,20 @@ where
     T: Copy,
 {
     fn new(base: T) -> Self {
-        Self { base, logs: vec![] }
+        Self {
+            base,
+            logs: vec![],
+            trace: vec![],
+        }
     }
 
     fn unwrap(&self) -> T {
         self.base
     }
 
-    fn add_logs(&mut self, opp_desc: String) {
-        self.logs.push(opp_desc)
+    fn add_logs(&mut self, opp_desc: String, curr_num: T) {
+        self.logs.push(opp_desc);
+        self.trace.push(curr_num);
     }
 }
 
@@ -24,17 +32,29 @@ fn square<T: std::ops::Mul<Output = T> + std::marker::Copy>(i: &T) -> MonadWrapp
 where
     for<'a> &'a T: std::ops::Mul<&'a T, Output = T>,
 {
-    let mut seg = MonadWrapper::new(i * i);
-    seg.add_logs("Squared the number".to_string());
+    let new_num = i * i;
+    let mut seg = MonadWrapper::new(new_num);
+    seg.add_logs("Squared the number".to_string(), new_num);
+    seg
+}
+
+fn times_two<T: std::ops::Add<Output = T> + std::marker::Copy>(i: &T) -> MonadWrapper<T>
+where
+    for<'a> &'a T: std::ops::Add<&'a T, Output = T>,
+{
+    let new_num = i + i;
+    let mut seg = MonadWrapper::new(new_num);
+    seg.add_logs("Multplied the number by two".to_string(), new_num);
     seg
 }
 
 fn add_one<T: std::ops::Add<Output = T> + std::marker::Copy>(i: &T) -> MonadWrapper<T>
 where
-    for<'a> &'a T: std::ops::Add<&'a T, Output = T>,
+    for<'a> &'a T: std::ops::Add<u128, Output = T>,
 {
-    let mut seg = MonadWrapper::new(i + i);
-    seg.add_logs("Added one to the number".to_string());
+    let new_num = i + 1;
+    let mut seg = MonadWrapper::new(new_num);
+    seg.add_logs("Added one to the number".to_string(), new_num);
     seg
 }
 
@@ -45,20 +65,38 @@ where
 {
     let mut new_num = trans_func(&base.unwrap());
     new_num.logs.append(&mut base.logs);
+    new_num.trace.append(&mut base.trace);
     MonadWrapper {
         base: new_num.unwrap(),
         logs: new_num.logs,
+        trace: new_num.trace,
     }
 }
 
 fn main() {
     let number = MonadWrapper::new(42);
-    let answer = transform(number, square);
-    let second_answer = transform(answer, add_one);
+    let mut first = transform(number, add_one);
+    let mut rng = rand::thread_rng();
+    for _ in 1..10 {
+      let x = rng.gen_range(1..=3);
+      match x {
+        1 => {
+           first = transform(first, add_one);
+        }
+        2 => {
+           first = transform(first, times_two);
+        }
+        3 => {
+           first = transform(first, square);
+        }
+        _ => panic!()
+      }
+    }
 
     println!(
-        "Answer is {}\nLogs are: {:?}",
-        second_answer.unwrap(),
-        second_answer.logs
+        "Answer is {}\nLogs are: {:?}\nTrace is: {:?}",
+      first.unwrap(),
+      first.logs,
+      first.trace
     );
 }
